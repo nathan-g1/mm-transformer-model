15fdb0753fb084381081538a548aea8bf08c342b
nathan-g1 <nathygech111@gmail.com>
2024-08-15 19:18:17 -0500

    Update converter

--- src/converter.py
+++ src/converter.py
@@ -1,29 +1,26 @@
+import os
 from osgeo import gdal
+from datetime import datetime
+# from dateutil.parser import parse
 
 options_list = [
     #'-ot Byte',
     '-of PNG'
     #'-b 1',
     #'-scale'
-]           
+]
 
 options_string = " ".join(options_list)
+tif_dir = "tif/"
+for i, filename in enumerate(os.listdir(tif_dir)):
+    filename_split = filename.split('_')
+    img_type = filename_split[len(filename_split) - 1]
+    date_string = filename_split[3]
+    date = datetime.strptime(date_string, '%Y%m%dT%H%M%S').date()
+    new_filename = str(i) + "_" + str(date) + "_" + img_type
 
-'''
-for i in range(33,65):
-    input = 's2_vis_'+ str(i) +'.tif'
-    output = 's2_vis_'+ str(i) +'.png'
-    #print(input)
-    #print(output)
     gdal.Translate(
-        output,
-        input,
+        os.path.join("png", new_filename),
+        os.path.join("tif", filename),
         options=options_string
     )
-'''
-
-gdal.Translate(
-    's2_vis_58_new.png',
-    's2_vis_58_new.tif',
-    options=options_string
-)
